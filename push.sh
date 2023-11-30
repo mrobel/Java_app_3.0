febb450627f185baa67d3a09adcec6cf24e5ca54
M Robel Miah <47643759+mrobel@users.noreply.github.com>
2023-11-30 22:45:34 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd target 
-curl -X PUT -u 'admin:Admin321' -T *.jar http://54.235.226.168:8082/artifactory/java-web-app/
+curl -X PUT -u 'admin:Admin321' -T *.jar http://3.83.143.61:8082/artifactory/java-web-app/
